36d4d0a5886bdf6ee66d9b9fac5a42caa9a55b72
aurorax <i@aurorax.org>
2013-12-08 22:54:21 +0800

    ixx.me url shortener 1.0

--- config.php
+++ config.php
@@ -12,10 +12,10 @@ $config['APP_MODULE_EMPTY'] = 'Index';
 $config['APP_ACTION_EMPTY'] = 'main';
 	
 $config['DB_TYPE'] = 'Mysql';		//数据库类型
-$config['DB_PREF'] = 'sp_';			//数据库前缀
+$config['DB_PREF'] = 'ixx_';			//数据库前缀
 $config['DB_HOST'] = 'localhost';	//数据库地址
 $config['DB_USER'] = 'root';		//账户
-$config['DB_PASS'] = 'rootpass';	//密码
+$config['DB_PASS'] = '';	//密码
 $config['DB_NAME'] = 'ixx';	//数据库名称
 $config['DB_PCON'] = false;			//false:非持续连接 true:持续连接
 $config['DB_CODE'] = 'utf8';		//数据编码
